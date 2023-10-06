8dcd5b0441ddc9844f672b148c67f600af90341f
sxck1337 <sxckv3@gmail.com>
2023-10-06 03:48:47 +0200

    Add files via upload

---
+++ IATHook.cpp
@@ -0,0 +1,121 @@
+#include "IATHook.hpp"
+
+std::vector<IAT_ENTRY> IAT_HOOK::hooks;
+
+IAT_STATUS IAT_HOOK::HookAPI( LPCSTR module_name, LPCSTR function_name, LPVOID detour, LPVOID* original, LPCSTR target_module, int type )
+{
+	LPVOID image_base = GetModuleHandleA( target_module );
+	PIMAGE_DOS_HEADER dos_headers = (PIMAGE_DOS_HEADER)image_base;
+	PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)((uintptr_t)image_base + dos_headers->e_lfanew);
+
+	IMAGE_DATA_DIRECTORY imports_directory = nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+	PIMAGE_IMPORT_DESCRIPTOR import_descriptor = (PIMAGE_IMPORT_DESCRIPTOR)(imports_directory.VirtualAddress + (uintptr_t)image_base);
+
+	while (import_descriptor->Name != NULL)
+	{
+		LPCSTR library_name = (LPCSTR)import_descriptor->Name + (uintptr_t)image_base;
+
+		if (_stricmp( library_name, module_name ) != NULL)
+		{
+			import_descriptor++;
+			continue;
+		}
+
+		PIMAGE_THUNK_DATA image_org_thunk_data = (PIMAGE_THUNK_DATA)((uintptr_t)image_base + import_descriptor->OriginalFirstThunk);
+		PIMAGE_THUNK_DATA image_thunk_data = (PIMAGE_THUNK_DATA)((uintptr_t)image_base + import_descriptor->FirstThunk);
+
+		while (image_org_thunk_data->u1.AddressOfData != NULL)
+		{
+			PIMAGE_IMPORT_BY_NAME import_data = (PIMAGE_IMPORT_BY_NAME)((uintptr_t)image_base + image_org_thunk_data->u1.AddressOfData);
+
+			if (strcmp( function_name, import_data->Name ) == NULL)
+			{
+				DWORD unused_protect = 0;
+				MEMORY_BASIC_INFORMATION mbi;
+
+				VirtualQuery( image_thunk_data, &mbi, sizeof( MEMORY_BASIC_INFORMATION ) );
+				if (!VirtualProtect( mbi.BaseAddress, mbi.RegionSize, PAGE_READWRITE, &mbi.Protect ))
+					return IAT_PROTECT;
+
+				LPVOID orig_func = (LPVOID)image_thunk_data->u1.Function;
+
+				image_thunk_data->u1.Function = (uintptr_t)detour;
+
+				if (!VirtualProtect( mbi.BaseAddress, mbi.RegionSize, mbi.Protect, &unused_protect ))
+					return IAT_RESTORE;
+
+				if (type == 0)
+				{
+					if (original != NULL)
+						*original = orig_func;
+
+					IAT_ENTRY new_entry{};
+					new_entry.detour = detour;
+					new_entry.function_name = function_name;
+					new_entry.module_name = module_name;
+					new_entry.original = orig_func;
+					new_entry.target_module = target_module;
+
+					IAT_HOOK::hooks.push_back( new_entry );
+				}
+				else
+				{
+					IAT_HOOK::hooks.erase( std::remove_if( IAT_HOOK::hooks.begin( ), IAT_HOOK::hooks.end( ),
+						[&function_name]( const IAT_ENTRY& entry ) {
+							return entry.function_name == function_name;
+						}
+					) );
+				}
+
+				return IAT_OK;
+			}
+
+			image_org_thunk_data++;
+			image_thunk_data++;
+		}
+
+		import_descriptor++;
+	}
+
+	return IAT_UNKNOWN;
+}
+
+IAT_STATUS IAT_HOOK::Create( LPCSTR module_name, LPCSTR function_name, LPVOID detour, LPVOID* original, LPCSTR target_module )
+{
+	if (!IAT_HOOK::hooks.empty( ))
+	{
+		for (auto& entry : IAT_HOOK::hooks)
+		{
+			if (strcmp( entry.function_name, function_name ) == NULL)
+				return IAT_DUPLICATE;
+		}
+	}
+
+	IAT_STATUS status = HookAPI( module_name, function_name, detour, original, target_module );
+
+	if (status != IAT_UNKNOWN)
+		return status;
+
+	return IAT_NOTFOUND;
+}
+
+IAT_STATUS IAT_HOOK::Restore( LPCSTR function_name )
+{
+	if (IAT_HOOK::hooks.empty( ))
+		return IAT_NOENTRY;
+
+	for (auto& entry : IAT_HOOK::hooks)
+	{
+		if (strcmp( entry.function_name, function_name ) == NULL)
+		{
+			IAT_STATUS status = HookAPI( entry.module_name, entry.function_name, entry.original, NULL, entry.target_module, 1 );
+
+			if (status != IAT_UNKNOWN)
+				return status;
+
+			return IAT_NOTFOUND;
+		}
+	}
+
+	return IAT_NOENTRY;
+}
---
+++ IATHook.hpp
@@ -0,0 +1,78 @@
+// IATHook 
+//
+// Copyright (c) 2023 sxck1337
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef IAT_HOOK_H
+#define IAT_HOOK_H
+
+#include <Windows.h>
+#include <vector>
+
+#if !(defined _M_IX86) && !(defined _M_X64) && !(defined __i386__) && !(defined __x86_64__)
+#error IAT_HOOK only supports x86/x64.
+#endif
+
+enum IAT_STATUS
+{
+	IAT_UNKNOWN = -1,		// used internally
+	IAT_OK,				// success 
+	IAT_PROTECT,		// failed to protect
+	IAT_RESTORE,		// failed to restore old protection
+	IAT_NOTFOUND,		// module name or function not found
+	IAT_DUPLICATE,		// function name already hooked
+	IAT_NOENTRY			// no hook found with that name
+};
+
+struct IAT_ENTRY
+{
+	LPCSTR module_name;
+	LPCSTR function_name;
+	LPVOID detour;
+	LPVOID original;
+	LPCSTR target_module;
+};
+
+class IAT_HOOK
+{
+public:
+	// creates a hook for the specified detour
+	// parameters:
+	//   module_name      [in]  name of the module containing the function.      
+	//
+	//   function_name    [in]  name of the function to hook.        
+	//
+	//	 detour			  [in]	pointer to your hook.          
+	//
+	//   original		  [out] pointer of the original function. (this parameter can be NULL if not needed)
+	//						
+	//   target_module    [in]  name of the module you want to target. (NULL by default, targets the main module of the process)
+	static IAT_STATUS Create( LPCSTR module_name, LPCSTR function_name, LPVOID detour, LPVOID* original, LPCSTR target_module = NULL );
+
+	// restores a previously hooked function to its original.
+	// parameters:
+	//   function_name    [in]  name of the function to restore.
+	static IAT_STATUS Restore( LPCSTR function_name );
+private:
+	static std::vector<IAT_ENTRY> hooks;
+	static IAT_STATUS HookAPI( LPCSTR module_name, LPCSTR function_name, LPVOID detour, LPVOID* original, LPCSTR target_module = NULL, int type = 0 );
+};
+
+#endif
